9d8d88d209aa75e10aea10170ec8f540339d6d99
Giordano Martín Díaz Merino <diazgiordano@gmail.com>
2024-04-10 16:19:20 -0500

    Add categories under admin flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
+  
   devise_for :admins
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
   root "home#index"
 
   # Defines the /admin path route
@@ -14,5 +8,9 @@ Rails.application.routes.draw do
     root to: "admin#index", as: :admin_root
   end
 
+  namespace :admin, path: '', as: '' do
+    resources :categories
+  end
+  
   get "admin" => "admin#index"
 end
