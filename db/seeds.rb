619780c66bb0ef19b32a994e60f81f53db830f44
Giordano Martín Díaz Merino <diazgiordano@gmail.com>
2024-06-28 09:08:18 -0500

    Add new config to render-build and update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
 Category.destroy_all
+Admin.destroy_all
+
+Admin.create(email: "admin@gmail.com", password: "123456")
 
 10.times do |i|
   Category.create(name: "Category #{i + 1}", description: "Description #{i + 1}")
